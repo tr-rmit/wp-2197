473023454e3a17843d47cbc8574e6bc263a23fc4
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2022-02-15 10:24:12 +1100

    Typo: = not ==

--- a3/post-validation.php
+++ a3/post-validation.php
@@ -7,12 +7,12 @@
 function validateBooking() {
   $errors = []; // new empty array to return error messages
   if ($_POST['user']['name'] == '') {
-    $errors['user']['name'] == "Name can't be blank";
+    $errors['user']['name'] = "Name can't be blank";
   } else {
     // more advanced checks here
   }
   if ($_POST['user']['email'] == '') {
-    $errors['user']['email'] == "Email can't be blank";
+    $errors['user']['email'] = "Email can't be blank";
   } else {
     // more advanced checks here
   }
